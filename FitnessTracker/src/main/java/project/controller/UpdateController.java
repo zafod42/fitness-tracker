f1e740c4c003ef556ebbc28c252c23b6ebf2afd0
max-yats <133808340+max-yats@users.noreply.github.com>
2024-05-10 21:45:45 +0300

    Update UpdateController.java
    Обновлен registerUser

--- FitnessTracker/src/main/java/project/controller/UpdateController.java
+++ FitnessTracker/src/main/java/project/controller/UpdateController.java
@@ -37,7 +37,8 @@ public class UpdateController {
 		
 		if (update.hasMessage()) {
 			defineCommand(update.getMessage());
-		} else if (update.hasCallbackQuery()) {
+		}
+		else if (update.hasCallbackQuery()) {
             String callbackData = update.getCallbackQuery().getData();
             
             SendMessage response = new SendMessage();
@@ -51,17 +52,19 @@ public class UpdateController {
             	response.setChatId(update.getCallbackQuery().getMessage().getChatId());
             	response.setText("(Тестовое) Вы нажали \"нет\"");
                 bot.sendAnswerMessage(response);
-            } else {
+            }
+			else {
 			log.error("Unsupported message type is received: " + update);
             }
 		}
 	}
 	
-	private void describeExercise(String command, Message msg)
-	{
+	private void describeExercise(String command, Message msg) {
 		SendMessage response = new SendMessage();
 		response.setChatId(msg.getChatId().toString());
-		response.setText(bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getName() + "\n" + bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getDescription() + "\n\nДобавить упражнение в вашу тренировку?");
+		response.setText(bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getName() + "\n" +
+				bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getDescription()
+				+ "\n\nДобавить упражнение в вашу тренировку?");
 		
 		InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
@@ -112,8 +115,15 @@ public class UpdateController {
 				viewStat();
 				break;
 			}
-		}
-		
+		}	
+	}
+
+	private void sendWelcomeMessage(Message msg) {
+		SendMessage response = new SendMessage();
+		response.setChatId(msg.getChatId().toString());
+		response.setText("Привет! Я бот для контроля за спортивными результатами в тренировках. " +
+				"Отправьте команду /tren, чтобы посмотреть доступные упражнения.");
+		bot.sendAnswerMessage(response);
 	}
 
 	public void viewExercises(Message msg) {
@@ -121,7 +131,8 @@ public class UpdateController {
 		  String helpStr = new String();
 		  sendMessage.setChatId(msg.getChatId());
 		  for (HashMap.Entry<Integer, Exercise> entry : bot.getExercises().getExerciseMap().entrySet()) {
-			  helpStr += "<a href='" + "https://t.me/FitTrackDomovonokBot?start=" + entry.getKey().toString() + "'>" + entry.getValue().getName() + "</a>\n";
+			  helpStr += "<a href='" + "https://t.me/FitTrackDomovonokBot?start=" + entry.getKey().toString() + "'>"
+					  + entry.getValue().getName() + "</a>\n";
 		  }
 		  sendMessage.setText(helpStr);
 		  sendMessage.enableHtml(true);
@@ -146,30 +157,23 @@ public class UpdateController {
 	private JdbcTemplate jdbcTemplate;
 
 	public void registerUser(Message msg) {
-		String chatId = msg.getChatId().toString();
-		// Проверяем, зарегистрирован ли уже пользователь
-		Integer userId;
-		try {
-			userId = jdbcTemplate.queryForObject("SELECT id FROM users WHERE chat_id = ?", new Object[]{chatId}, Integer.class);
-		} catch (EmptyResultDataAccessException e) {
-			userId = null; // Пользователь не найден
-		}
+		String chatIdStr = msg.getChatId().toString();
+		Long chatId = Long.parseLong(chatIdStr);
 
-		if (userId == null) {
-			// Пользователь не найден, регистрируем его
+		// Проверяем, зарегистрирован ли уже пользователь
+		List<Long> userIds = jdbcTemplate.queryForList("SELECT id FROM users WHERE chat_id =?", new Object[]{chatId}, Long.class);
+
+		if (userIds.isEmpty()) {
 			jdbcTemplate.update("INSERT INTO users (chat_id) VALUES (?)", chatId);
-			// Отправляем сообщение о регистрации
 			SendMessage response = new SendMessage();
-			response.setChatId(chatId);
-			response.setText("Вы успешно зарегистрированы!");
+			response.setChatId(chatIdStr);
+			response.setText("Счастилового путешествия в Казахстан");
 			bot.sendAnswerMessage(response);
 		} else {
-			// Пользователь уже зарегистрирован, предлагаем начать тренировку
 			SendMessage response = new SendMessage();
-			response.setChatId(chatId);
-			response.setText("Вы уже зарегистрированы!");
+			response.setChatId(chatIdStr);
+			response.setText("АMOGUS!");
 			bot.sendAnswerMessage(response);
 		}
 	}
-
 }
