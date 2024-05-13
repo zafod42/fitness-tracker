f3edc634870f7a49671738e4294f3b614c4ef843
max-yats <133808340+max-yats@users.noreply.github.com>
2024-05-13 17:23:59 +0300

    Update UpdateController.java
    1. Добавлена возможность удаления пользователя (deleteUser и confirmAccountDeletion) 
    2. Слегка реализован метод просмотра статиститики (viewStat)

--- FitnessTracker/src/main/java/project/controller/UpdateController.java
+++ FitnessTracker/src/main/java/project/controller/UpdateController.java
@@ -3,10 +3,10 @@ package project.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -15,10 +15,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import project.model.CardioExercise;
 import project.model.Exercise;
-import project.model.FlexibleExercise;
-import project.model.StrengthExercise;
 
 @Component
 public class UpdateController {
@@ -43,12 +40,12 @@ public class UpdateController {
             
             SendMessage response = new SendMessage();
 
-            if(callbackData.equals("YES_BUTTON")){
+            if (callbackData.equals("YES_BUTTON")) {
             	response.setText("(Тестовое) Вы нажали \"да\"");
             	response.setChatId(update.getCallbackQuery().getMessage().getChatId());
                 bot.sendAnswerMessage(response);
             }
-            else if(callbackData.equals("NO_BUTTON")){
+            else if (callbackData.equals("NO_BUTTON")) {
             	response.setChatId(update.getCallbackQuery().getMessage().getChatId());
             	response.setText("(Тестовое) Вы нажали \"нет\"");
                 bot.sendAnswerMessage(response);
@@ -63,8 +60,8 @@ public class UpdateController {
 		SendMessage response = new SendMessage();
 		response.setChatId(msg.getChatId().toString());
 		response.setText(bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getName() + "\n" +
-				bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getDescription()
-				+ "\n\nДобавить упражнение в вашу тренировку?");
+						bot.getExercises().getExerciseMap().get(Integer.valueOf(command)).getDescription()
+							+ "\n\nДобавить упражнение в вашу тренировку?");
 		
 		InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
@@ -103,6 +100,9 @@ public class UpdateController {
 			switch(command)
 			{
 			case "/start":
+				sendWelcomeMessage(msg);
+				break;
+			case "/register":
 				registerUser(msg);
 				break;
 			case "/tren":
@@ -112,10 +112,16 @@ public class UpdateController {
 				testOutput(msg);
 				break;
 			case "/stat":
-				viewStat();
+				viewStat(msg);
+				break;
+			case "/delete":
+				confirmAccountDeletion(msg);
+				break;
+			case "ПОДТВЕРЖДАЮ УДАЛЕНИЕ":
+				deleteUser(msg);
 				break;
 			}
-		}	
+		}
 	}
 
 	private void sendWelcomeMessage(Message msg) {
@@ -143,15 +149,55 @@ public class UpdateController {
 	public void testOutput(Message msg) {
 		SendMessage response = new SendMessage();
 		response.setChatId(msg.getChatId().toString());
-		response.setText("Тестовое сообщение от бота");
+		response.setText("Тестовое сообщение - Цой жив");
 		bot.sendAnswerMessage(response);
 		log.debug(msg.getText());
 	}
-	
-	public void viewStat() {
-		//тут должна быть функция, что ищет в базе данных Id чата
-		//и по этому id выводит статистику пользователя из БД
-	}
+
+	public void viewStat(Message msg) {
+		String chatIdStr = msg.getChatId().toString();
+		Long chatId = Long.parseLong(chatIdStr);
+
+		String sql = "SELECT exercises, info FROM users WHERE chat_id =?";
+
+		try {
+			List<Map<String, Object>> results = jdbcTemplate.queryForList(sql, chatId);
+
+			if (!results.isEmpty()) {
+				Map<String, Object> result = results.get(0);
+				Integer[] exercises = (Integer[]) result.get("exercises");
+				String info = (String) result.get("info");
+
+				StringBuilder statMessage = new StringBuilder("Ваша статистика:\n");
+				statMessage.append("Информация: ").append(info).append("\n");
+				statMessage.append("Выполненные упражнения: \n");
+
+				for (Integer exerciseId : exercises) {
+					// Здесь можно добавить дополнительный запрос для получения названия упражнения по ID
+					// Для примера просто выводим ID упражнения
+					statMessage.append("- Упражнение ID: ").append(exerciseId).append("\n");
+				}
+
+				SendMessage response = new SendMessage();
+				response.setChatId(chatIdStr);
+				response.setText(statMessage.toString());
+				bot.sendAnswerMessage(response);
+			}
+			else {
+				SendMessage response = new SendMessage();
+				response.setChatId(chatIdStr);
+				response.setText("Статистика не найдена.");
+				bot.sendAnswerMessage(response);
+			}
+		}
+		catch (Exception e) {
+			log.error("Ошибка при получении статистики пользователя: " + e.getMessage());
+			SendMessage response = new SendMessage();
+			response.setChatId(chatIdStr);
+			response.setText("Произошла ошибка при получении статистики.");
+			bot.sendAnswerMessage(response);
+		}
+	}	
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -160,20 +206,41 @@ public class UpdateController {
 		String chatIdStr = msg.getChatId().toString();
 		Long chatId = Long.parseLong(chatIdStr);
 
-		// Проверяем, зарегистрирован ли уже пользователь
-		List<Long> userIds = jdbcTemplate.queryForList("SELECT id FROM users WHERE chat_id =?", new Object[]{chatId}, Long.class);
+		List<Long> userIds = jdbcTemplate.queryForList("SELECT id FROM users WHERE chat_id =?", 
+				new Object[]{chatId}, Long.class);
 
 		if (userIds.isEmpty()) {
 			jdbcTemplate.update("INSERT INTO users (chat_id) VALUES (?)", chatId);
 			SendMessage response = new SendMessage();
 			response.setChatId(chatIdStr);
-			response.setText("Счастилового путешествия в Казахстан");
+			//response.setText("Регистрация прошла успешно!");
+			response.setText("Счастилового путешествия в Казахстан!");
 			bot.sendAnswerMessage(response);
-		} else {
+		} 
+		else {
 			SendMessage response = new SendMessage();
 			response.setChatId(chatIdStr);
+			//response.setText("Вы уже зарегистрированы!");
 			response.setText("АMOGUS!");
 			bot.sendAnswerMessage(response);
 		}
+	}
+
+	private void confirmAccountDeletion(Message msg) {
+		SendMessage response = new SendMessage();
+		response.setChatId(msg.getChatId().toString());
+		response.setText("Вы уверены, что хотите удалить свой аккаунт? Отправьте 'Подтверждаю удаление' для подтверждения.");
+		bot.sendAnswerMessage(response);
+	}
+
+	private void deleteUser(Message msg) {
+		String chatIdStr = msg.getChatId().toString();
+		Long chatId = Long.parseLong(chatIdStr);
+		jdbcTemplate.update("DELETE FROM users WHERE chat_id =?", chatId);
+		SendMessage response = new SendMessage();
+		response.setChatId(chatIdStr);
+		//response.setText("Ваш аккаунт был успешно удалён.");
+		response.setText("Попутного ветра!");
+		bot.sendAnswerMessage(response);
 	}
 }
