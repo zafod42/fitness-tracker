174c7e85c188b675785036615d78d3f1319ab920
max-yats <133808340+max-yats@users.noreply.github.com>
2024-05-13 17:28:32 +0300

    Update FitnessBot.java

--- FitnessTracker/src/main/java/project/controller/FitnessBot.java
+++ FitnessTracker/src/main/java/project/controller/FitnessBot.java
@@ -9,9 +9,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import project.model.TrainingLibrary;
-
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,44 +17,47 @@ import javax.annotation.PostConstruct;
 import org.apache.log4j.Logger;
 
 @Component
-public class FitnessBot extends TelegramLongPollingBot{
+public class FitnessBot extends TelegramLongPollingBot {
 	@Value("${bot.name}")
 	private String name = "FitTrackDomovonokBot";
 	@Value("${bot.token}")
 	private String token = "7033585733:AAGXgDOBCO3R9lz2XX1HVVGOWR_hcfThNds";
 	private final UpdateController controller;
-	
+
 	@Getter
 	private TrainingLibrary Exercises = new TrainingLibrary();
-	
+
 	private static final Logger log = org.apache.log4j.Logger.getLogger(FitnessBot.class);
-	
+
 	public FitnessBot(UpdateController controller) {
 		this.controller = controller;
 		Exercises.initialize();
-		
-		List<BotCommand> listofCommands = new ArrayList<>();
-        listofCommands.add(new BotCommand("/start", "уточните график тренеровок"));
-        listofCommands.add(new BotCommand("/tren", "список доступных упражнений"));
-        listofCommands.add(new BotCommand("/test", "тестовая команда"));
-        listofCommands.add(new BotCommand("/stat", "ваша статистика"));
-        try {
-            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
-        } catch (TelegramApiException e) {
-            log.error("Error setting bot's command list: " + e.getMessage());
-        }
-	}
 	
+		List<BotCommand> listOfCommands = new ArrayList<>();
+	        listOfCommands.add(new BotCommand("/start", "уточните график тренировок"));
+	        listOfCommands.add(new BotCommand("/tren", "список доступных упражнений"));
+	        listOfCommands.add(new BotCommand("/test", "тестовая команда"));
+	        listOfCommands.add(new BotCommand("/stat", "ваша статистика"));
+		listOfCommands.add(new BotCommand("/delete", "удаление аккаунта"));
+	
+	        try {
+	        	this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+	        }
+		catch (TelegramApiException e) {
+	        	log.error("Error setting bot's command list: " + e.getMessage());
+	        }
+	}
+
 	@PostConstruct
 	public void init () {
 		controller.registerBot(this);
 	}
-	
-	@Override 
+
+	@Override
 	public String getBotUsername() {
 		return name;
 	}
-	
+
 	@Override
 	public String getBotToken() {
 		return token;
@@ -66,15 +67,15 @@ public class FitnessBot extends TelegramLongPollingBot{
 	public void onUpdateReceived(Update update) {
 		controller.processUpdate(update);
 	}
-	
+
     public void sendAnswerMessage(SendMessage message) {
         if (message != null) {
             try {
                 execute(message);
-            } catch (TelegramApiException e) {
+            }
+			catch (TelegramApiException e) {
                 log.error(e);
             }
         }
     }
-	
 }
