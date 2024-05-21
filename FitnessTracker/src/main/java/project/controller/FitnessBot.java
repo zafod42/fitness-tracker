bde97505fc7884d56df139d124345f35d0b75621
max-yats <133808340+max-yats@users.noreply.github.com>
2024-05-21 23:55:22 +0300

    Added methods /finishSet и /stop in the listOfCommands

--- FitnessTracker/src/main/java/project/controller/FitnessBot.java
+++ FitnessTracker/src/main/java/project/controller/FitnessBot.java
@@ -1,6 +1,9 @@
 package project.controller;
+import project.model.*;
 
 import lombok.Getter;
+import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,42 +12,46 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import project.model.TrainingLibrary;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import org.apache.log4j.Logger;
 
 @Component
 public class FitnessBot extends TelegramLongPollingBot {
 	@Value("${bot.name}")
 	private String name;
 	@Value("${bot.token}")
-	private String token;
+	private String token";
+
 	private final UpdateController controller;
 
 	@Getter
 	private TrainingLibrary Exercises = new TrainingLibrary();
 
-	private static final Logger log = org.apache.log4j.Logger.getLogger(FitnessBot.class);
+	static final Logger log = org.apache.log4j.Logger.getLogger(FitnessBot.class);
 
 	public FitnessBot(UpdateController controller) {
 		this.controller = controller;
 		Exercises.initialize();
+
 		List<BotCommand> listOfCommands = new ArrayList<>();
-		listOfCommands.add(new BotCommand("/start", "запустить фитнес-трекер"));
+        	listOfCommands.add(new BotCommand("/start", "запустить фитнес-трекер"));
 		listOfCommands.add(new BotCommand("/register", "зарегистрироваться"));
-		listOfCommands.add(new BotCommand("/tren", "вывод списка доступных упражнений"));
-		listOfCommands.add(new BotCommand("/test", "тестовая команда"));
-		listOfCommands.add(new BotCommand("/stat", "ваша статистика"));
-		listOfCommands.add(new BotCommand("/delete", "удаление аккаунта"));
+        	listOfCommands.add(new BotCommand("/tren", "вывод списка доступных упражнений"));
+        	listOfCommands.add(new BotCommand("/test", "тестовая команда"));
+        	listOfCommands.add(new BotCommand("/stat", "ваша статистика"));
+		listOfCommands.add(new BotCommand("/startExercise", "начать упражнение"));
+		listOfCommands.add(new BotCommand("/finishSet", "закончить подход"));
+		listOfCommands.add(new BotCommand("/stop", "досрочно завершить упражнение"));
 
 		try {
-			this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
-		} catch (TelegramApiException e) {
-			log.error("Error setting bot's command list: " + e.getMessage());
-		}
+            		this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        	}
+		catch (TelegramApiException e) {
+            		log.error("Error setting bot's command list: " + e.getMessage());
+        	}
 	}
 
 	@PostConstruct
