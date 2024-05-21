eb2969a8795ed8b1a3eb8572ec7a4bb44d0e713e
max-yats <133808340+max-yats@users.noreply.github.com>
2024-05-22 01:43:33 +0300

    Added exercises from TrainingLibrary.java

--- FitnessTracker/db.sql
+++ FitnessTracker/db.sql
@@ -39,10 +39,34 @@ SET row_security = off;
 SET default_tablespace = '';
 SET default_table_access_method = heap;
 
-CREATE TABLE IF NOT EXISTS public.exercises (
+-- Обычные упражнения
+CREATE TABLE IF NOT EXISTS public.ordinaryExercise (
     id numeric(12,0) PRIMARY KEY,
     name text[],
     description text[],
+    sets integer,
+    repetitions integer,
+    type text[]
+);
+
+-- Упражнения на время
+CREATE TABLE IF NOT EXISTS public.timeExercise (
+    id numeric(12,0) PRIMARY KEY,
+    name text[],
+    description text[],
+    sets integer,
+    timeInSeconds real,
+    type text[]
+);
+
+-- Упражнения c весом
+CREATE TABLE IF NOT EXISTS public.weightExercise (
+    id numeric(12,0) PRIMARY KEY,
+    name text[],
+    description text[],
+    sets integer,
+    repetitions integer,
+    weightPerRep real,
     type text[]
 );
 
@@ -58,24 +82,53 @@ ALTER TABLE public.users
 
 COMMENT ON TABLE public.users IS 'Пользователи';
 
-INSERT INTO public.exercises (id, name, description, type)
-VALUES (3001, '{"Прыжки в длину"}', '{"очень полезное упражнение - мамой клянусь"}', '{Flexible}')
+-- Обычные упражнения
+INSERT INTO public.ordinaryExercise (id, name, description, sets, repetitions, type)
+VALUES (1001, '{"Приседания"}', '{"упражнение для ног и ягодиц, выполняется, опускаясь в положение, как будто вы садитесь на стул, а затем поднимаетесь."}', 3, 20, '{Ordinary}')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.exercises (id, name, description, type)
-VALUES (1001, '{"Планка на прямых руках"}', '{"статическая нагрузка мышц груди"}', '{Strength}')
+INSERT INTO public.ordinaryExercise (id, name, description, sets, repetitions, type)
+VALUES (1002, '{"Подтягивания"}', '{"упражнение для верхней части тела, выполняется подтягиванием тела вверх, держась за перекладину."}', 2, 5, '{Ordinary}')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.exercises (id, name, description, type)
-VALUES (1002, '{Отжимания}', '{"упражнение для верхней части тела. Выполняется","когда лицо опущено вниз","и руки отталкивают тело от земли."}', '{Strength}')
+INSERT INTO public.ordinaryExercise (id, name, description, sets, repetitions, type)
+VALUES (1003, '{"Прыжки на скакалке"}', '{"кардиоупражнение, которое также тренирует координацию, выполняется с прыжками через скакалку."}', 3, 90, '{Ordinary}')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.exercises (id, name, description, type)
-VALUES (1003, '{Приседания}', '{"упражнение для ног и ягодиц",выполняется,"опускаясь в положение","как будто вы садитесь на стул","а затем поднимаетесь."}', '{Strength}')
+INSERT INTO public.ordinaryExercise (id, name, description, sets, repetitions, type)
+VALUES (1004, '{"Махи ногами"}', '{"упражнение для ног и ягодиц, выполняется махая ногой вперед и назад."}', 3, 20, '{Ordinary}')
 ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO public.exercises (id, name, description, type)
-VALUES (2001, '{"Бег на месте"}', '{кардиоупражнение,"которое можно выполнять дома","бегая на месте."}', '{Cardio}')
+INSERT INTO public.ordinaryExercise (id, name, description, sets, repetitions, type)
+VALUES (1005, '{"Лодка"}', '{"упражнение для спины и пресса, выполняется, лежа на животе и поднимая туловище и ноги от пола, формируя форму лодки."}', 3, 30, '{Ordinary}')
+ON CONFLICT (id) DO NOTHING;
+
+
+-- Упражнения на время
+INSERT INTO public.timeExercise (id, name, description, sets, timeInSeconds, type)
+VALUES (2001, '{"Планка на прямых руках"}', '{"статическая нагрузка мышц груди"}', 1, 10.0, '{Time}')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.timeExercise (id, name, description, sets, timeInSeconds, type)
+VALUES (2002, '{"Прыжки на скакалке"}', '{"кардиоупражнение, которое также тренирует координацию, выполняется с прыжками через скакалку."}', 3, 10.0, '{Time}')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.timeExercise (id, name, description, sets, timeInSeconds, type)
+VALUES (2003, '{"Бег на месте"}', '{"кардиоупражнение, которое можно выполнять дома, бегая на месте"}', 2, 30.0, '{Time}')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.timeExercise (id, name, description, sets, timeInSeconds, type)
+VALUES (2004, '{"Планка"}', '{"упражнение для укрепления кора"}', 2, 90.0, '{Time}')
+ON CONFLICT (id) DO NOTHING;
+
+
+-- Упражнения c весом
+INSERT INTO public.weightExercise (id, name, description, sets, repetitions, weightPerRep, type)
+VALUES (3001, '{"Жим лежа"}', '{"упражнение для развития грудных мышц"}', 2, 10, 25.0, '{Weight}')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.weightExercise (id, name, description, sets, repetitions, weightPerRep, type)
+VALUES (3002, '{"Подъём гантели"}', '{"чтоб бицуху качать"}', 6, 10, 10.0, '{Weight}')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.users (exercises, info, chat_id) VALUES (ARRAY[1001, 1002], 'some info', 123456);
